25134940a44f1fda3b29a814809a7c3494c883de
Nazmus Sakib <nazmussakib@MacBook-Pro.local>
2020-06-26 16:44:17 +0200

    added latest changes]

--- src/app/customer-order-info/customer-order-info.component.ts
+++ src/app/customer-order-info/customer-order-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import Front from '@frontapp/plugin-sdk';
 
 @Component({
   selector: 'app-customer-order-info',
@@ -12,79 +13,110 @@ export class CustomerOrderInfoComponent implements OnInit {
 
   customers: any = [];
   selectedCustomers: any = [];
-  contentLoading = true;
   individualLoading = true;
+  contentLoading = false;
+  searchText = '';
+  currentLoading = 0;
 
-  constructor(private api: ApiService, private router: Router) {}
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
-    
-    this.initCustomers()
-  }
-
-  initCustomers = () => {
-    this.api.getCustomerEmails()
-      .subscribe(
-        res => {
-          res.customer.forEach(customer => {
-            customer.loading = true
-            this.customers.push(customer);
-            this.selectedCustomers.push(customer);
-          }),
-            error => console.log(error)
-        }).add(() => {
-          this.contentLoading = false;
+    Front.contextUpdates.subscribe(context => {
+      this.contentLoading = false;
+      this.customers = []
+      switch (context.type) {
+        case 'noConversation':
+          console.log('No conversation selected');
+          break;
+        case 'singleConversation':
+          this.individualLoading = true;
+          this.customers.push({ email: (context as any).conversation.recipient.handle });
           this.getOrders();
-        });
+          break;
+        case 'multiConversations':
+          this.individualLoading = true;
+          (context as any).conversations.forEach(conversations => {
+            this.customers.push({ email: conversations.recipient.handle });
+            this.getOrders();
+          });
+          break;
+        default:
+          console.error(`Error: ${context.type}`);
+          break;
+      }
+    });
   }
 
   getOrders = () => {
+    this.currentLoading += 1;
     this.customers.forEach((customer, idx) => {
-      
       this.api.getCustomerInfo(customer.email)
         .subscribe(
           res => {
-            this.customers[idx].info = res.data.customer
-            this.customers[idx].name = `${res.data.customer.first_name} ${res.data.customer.last_name}`
-            this.customers[idx].tlds = res.data.tlds
-            this.customers[idx].phn = res.data.customer.phone
-            this.customers[idx].postcode = res.data.customer.postcode
-            this.customers[idx].company = res.data.customer.company
-            this.customers[idx].lastname = res.data.customer.last_name
-            this.customers[idx].orders = []
-
-            res.data.orders.forEach(orders => {
-              this.customers[idx].orders.push(orders)
-            })
-            this.customers[idx].loading = false
-            this.selectedCustomers[idx] = this.customers[idx],
-              error => console.log(error)
+            this.pushCustomerData(res, idx)
+            error => console.log(error)
           }).add(() => {
             this.individualLoading = false
+            this.currentLoading -= 1
           });
     });
+  }
+
+  pushCustomerData = (res, idx) => {
+    this.customers[idx].info = res.data.customer
+    this.customers[idx].name = `${res.data.customer.first_name} ${res.data.customer.last_name}`
+    this.customers[idx].tlds = res.data.tlds
+    this.customers[idx].phn = res.data.customer.phone
+    this.customers[idx].postcode = res.data.customer.postcode
+    this.customers[idx].address_1 = res.data.customer.address_1
+    this.customers[idx].address_2 = res.data.customer.address_2
+    this.customers[idx].city = res.data.customer.city
+    this.customers[idx].country = res.data.customer.country
+    this.customers[idx].company = res.data.customer.company
+    this.customers[idx].lastname = res.data.customer.last_name
+    this.customers[idx].orders = []
+
+    res.data.orders.forEach(orders => {
+      this.customers[idx].orders.push(orders)
+    })
+    this.customers[idx].loading = false
+    this.selectedCustomers[idx] = this.customers[idx]
   }
 
   openPost = (id, tld) => {
     let postId
     this.individualLoading = true;
     this.api.getPostId(id).subscribe(res => {
-        res.data === 0 ? postId = id: postId = res.data
-        window.open(`https://www.smoovall.${tld}/wp-admin/post.php?post=${postId}&action=edit`, "_blank"),
+      res.data === 0 ? postId = id : postId = res.data
+      window.open(`https://www.smoovall.${tld}/wp-admin/post.php?post=${postId}&action=edit`, "_blank"),
         error => console.log(error)
     })
     this.individualLoading = false;
   }
 
   seeConversations = (email, conversation) => {
-    this.router.navigateByUrl('/conversations', { state: { email, conversation}});
+    this.router.navigateByUrl('/conversations', { state: { email, conversation } });
   }
 
-  search = (event) => { //todo -> fix info
-    var value = event.target.value.toLowerCase()
-    this.selectedCustomers = this.customers.filter((customer) => customer.email?.toLowerCase().startsWith(value) 
-    || customer.name?.toLowerCase().startsWith(value) || customer.lastname?.toLowerCase().startsWith(value)
-    || customer.postcode?.toLowerCase().startsWith(value) || customer.phn?.toLowerCase().startsWith(value)
-    || customer.orders?.some(o => o.id?.toString().toLowerCase().startsWith(value)))
+  search = () => {
+    this.contentLoading = true;
+    if (this.searchText != '') {
+      this.customers = [];
+      this.api.getCustomerInfo(this.searchText)
+        .subscribe(
+          res => {
+            this.customers[0] = {}
+            this.pushCustomerData(res, 0)
+            error => console.log(error)
+          }).add(() => {
+            this.individualLoading = false;
+            this.contentLoading = false;
+          });
+    }
+    
+  }
+
+  saveInput = (event) => {
+    this.searchText = event.target.value.toLowerCase()
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  serverURL: 'http://localhost:3000'
+  serverURL: 'https://smoovall-front-plugin-backend.herokuapp.com'
 };
 
 
